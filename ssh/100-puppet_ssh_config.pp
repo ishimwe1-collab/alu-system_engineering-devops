6162fd906da39ccab629e3e7692f6b9a47485901
ishimwe1-collab <ishimwe1-collab@github.com>
2026-07-28 14:26:49 -0700

    Configure SSH client with Puppet

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Configure SSH client to use school private key and disable password authentication
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^ *IdentityFile ~/.ssh/school$',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^ *PasswordAuthentication no$',
+}
